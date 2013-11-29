be9376746b02507da1ce647d0e5c391aff485b26
AlexLong <henryprogi@hotmail.com>
2013-11-29 18:42:19 +0600

    Changed to Entity UserEntity to MulkUser

---
+++ MulkBulk.Domain/Entities/MulkUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MulkBulk.Domain.Entities
+{
+    public class MulkUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        private IList<UserMessages> _messages;
+
+        public virtual IList<UserMessages> Messages
+        {
+            get { return _messages ?? (_messages = new List<UserMessages>()); }
+            set { _messages = value; }
+        }
+
+        public DateTime RegistrationDate { get; set; }
+
+
+
+    }
+}
---
+++ MulkBulk/Migrations/201311291158402_AddDescriptionCategory.cs
@@ -0,0 +1,16 @@
+namespace MulkBulk.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDescriptionCategory : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
